6c7021a34ce6b793afb0f1c2ad58207cce636fa2
Z <zijianhuang2000@gmail.com>
2019-05-29 21:15:49 +1000

    not working well with Material Visual. Exception thrown in LoadApplication()

--- Demo2/Demo2.Android/MainActivity.cs
+++ Demo2/Demo2.Android/MainActivity.cs
@@ -7,19 +7,21 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using ClassLibrary1.Android;
+using Xamarin.Forms;
 namespace Demo2.Droid
 {
-    [Activity(Label = "Demo2", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            //TabLayoutResource = Resource.Layout.Tabbar;
-            //ToolbarResource = Resource.Layout.Toolbar;
+	[Activity(Label = "Demo2", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			//TabLayoutResource = Resource.Layout.Tabbar;
+			//ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-    }
+			base.OnCreate(savedInstanceState);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			FormsMaterial.Init(this, savedInstanceState);
+			LoadApplication(new App());
+		}
+	}
 }
